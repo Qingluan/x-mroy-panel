3936f769befff0405e5252775d3ce806afb33c73
dr <dr@users-MacBook-Pro.local>
2019-03-14 15:56:35 +0800

    Initial Commit

---
+++ Panel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Panel
+//
+//  Created by dr on 2019/3/14.
+//  Copyright © 2019 dr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ PanelTests/PanelTests.swift
@@ -0,0 +1,34 @@
+//
+//  PanelTests.swift
+//  PanelTests
+//
+//  Created by dr on 2019/3/14.
+//  Copyright © 2019 dr. All rights reserved.
+//
+
+import XCTest
+@testable import Panel
+
+class PanelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
